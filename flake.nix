a837ba6eb2cbaaa07e10b40af132b5773eb14c14
Akshay <nerdy@peppe.rs>
2021-08-23 19:22:45 +0530

    add package: sim-tools/converter

--- flake.nix
+++ flake.nix
@@ -2,19 +2,29 @@
   description = "Shadysim";
 
   inputs = {
+
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+
     pycrypto-src = {
       url = "github:pycrypto/pycrypto";
       flake = false;
     };
+
     simtools-src = {
       url = "git+https://git.osmocom.org/sim/sim-tools";
       flake = false;
     };
+
   };
 
 
-  outputs = { self, nixpkgs, pycrypto-src, simtools-src, ... }:
+  outputs =
+    { self
+    , nixpkgs
+    , pycrypto-src
+    , simtools-src
+    , ...
+    }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
@@ -31,10 +41,11 @@
 
       overlay = final: prev: rec {
 
-        pycrypto = with final.python2Packages;
+        pycrypto =
+          with final.python2Packages;
           buildPythonPackage {
             pname = "pycrypto";
-            version = "0.1.0";
+            version = "2.6.1";
             src = pycrypto-src;
           };
 
@@ -64,10 +75,32 @@
 
         shadysim-bin = with final; callPackage shadysim { };
 
+        converter-bin =
+          let
+            CLASSPATH = "${simtools-src}/javacard/bin/converter.jar";
+            JFLAGS = "-classpath ${CLASSPATH}";
+            jdk = final.adoptopenjdk-jre-hotspot-bin-8;
+          in
+          with final;
+          pkgs.runCommand
+            "converter"
+            {
+              inherit CLASSPATH;
+              inherit JFLAGS;
+            }
+            ''
+              mkdir -p $out/bin
+              cat << EOF > $out/bin/converter
+              ${jdk}/bin/java \
+                $JFLAGS com.sun.javacard.converter.Converter "$@"
+              EOF
+              chmod +x $out/bin/converter
+            '';
+
       };
 
       packages = forAllSystems (system: {
-        inherit (nixpkgsFor.${system}) shadysim-bin;
+        inherit (nixpkgsFor.${system}) shadysim-bin converter-bin;
       });
 
 
